3e2d97a25c621e4eec149728e847c45771a53cc5
AhmedAbdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-05-26 08:21:54 +0000

    multi view port

--- cypress/e2e/03-viewPort.cy.js
+++ cypress/e2e/03-viewPort.cy.js
@@ -1,14 +1,22 @@
 
+/* 
+    - this test case test multi view of the website 
+    - we use foreach loop to test all views and make code more usability 
+*/
 
-it('we should be able to open website in different views' , ()=>{
+['samsung-note9','ipad-mini','macbook-13'].forEach(viewport => {
+
+    it('we should be able to open website in different views '+viewport , ()=>{
     // -we use view port to test the width and hight of the test runner
     // -usefully to test our application in different viewport
-    
-    cy.viewport(550,750);
+    cy.viewport(viewport)
     cy.visit('http://10.0.2.15:8080/index.html');
 
-    cy.viewport('samsung-note9')
 
+    // - we can assign width and hight by the model of device  
+    
 
 }
 )
+})
+
